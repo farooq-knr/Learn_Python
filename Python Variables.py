2e0cc279aad503dba745b1209619ab457186047c
farooq-knr <farooq.knr@gmail.com>
2026-05-04 21:08:41 +0530

    program

---
+++ Python Variables.py
@@ -0,0 +1,29 @@
+print("Hello, World!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+if 6 < 5:
+    print("Six is greater than five!")
+else:   print("Six is not greater than five!")
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+x = "Python is great"
+
+x = "awesome"
+
+def myfunc():
+  print("==Start of function")
+  y = 10
+  print(f"Python is {y}")
+  print("==End of function-")
+print("Python is " + x)
+print("Python is " + str(y))
+
+myfunc()
+
